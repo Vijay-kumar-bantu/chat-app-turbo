886c148c6bc77023fc9d702e1b33a2f2e23389bc
Vijay <capvijju24@gmail.com>
2025-04-12 02:14:08 +0530

    added prettier for json files

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -19,7 +19,7 @@ export const config = [
         },
         rules: {
             "turbo/no-undeclared-env-vars": "warn",
-            "no-console": "error",
+            "no-console": ["warn", { allow: ["warn", "error"] }],
         },
     },
     {
